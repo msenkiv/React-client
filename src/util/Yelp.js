534c0ba40af0021dd4a3c22d014c98b1956e85b4
WINDOWS-FQ7SKPA\tania <taniasimone72@gmail.com>
2020-07-16 18:05:01 -0600

    upload

---
+++ src/util/Yelp.js
@@ -0,0 +1,41 @@
+const apiKey = "4jwGSPMKknnc2pLaSQwuo0DAac3aZUE0k42lN1oEx8n75bz98vzWDWzdTYK8cUx4G8cnzvLzflY_OthVI29UjeXcVwvB7GWGbnU6i4XPSMVxO9eFOp_mRZGiA28OX3Yx"
+
+const Yelp = {
+    searchYelp(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+
+
+            }
+        }).then((response) => {
+            return response.json();
+
+        }).then((jsonResponse) => {
+            if(jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    console.log(business);
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories [0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count, 
+                    }
+                });
+            }
+
+        }) 
+
+    
+
+    }
+
+}
+
+export default Yelp;
